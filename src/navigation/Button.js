b85a12a9ecb4086f2d0abbf153d795582088fca6
vedtra <vedtrapurba96@gmail.com>
2018-11-15 13:19:39 +0700

    week1

---
+++ src/navigation/Button.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Navlist from './Button.css';
+
+class button extends Component {
+  render() {
+    return (
+      <div class="navlist">
+      	 <li>
+                  <a href="" class="button">BELI</a><br /> <br />  <a href="" class="buttons">BANDINGKAN</a> 
+              </li>
+              <br />
+              <li>
+                  <a href="" class="button">BELI</a><br /> <br />  <a href="" class="buttons">BANDINGKAN</a>
+              </li>
+              <br />
+              <li>
+                  <a href="" class="button">BELI</a><br /> <br />  <a href="" class="buttons">BANDINGKAN</a>
+              </li>
+              <br />
+              <li>
+                  <a href="" class="button">BELI</a><br /> <br />  <a href="" class="buttons">BANDINGKAN</a>
+              </li>
+              <br />
+
+              <li>
+                  <a href="" class="button">BELI</a><br /> <br />  <a href="" class="buttons">BANDINGKAN</a>
+              </li>
+              <br />
+              <li>
+                  <a href="" class="button">BELI</a><br /> <br />  <a href="" class="buttons">BANDINGKAN</a>
+              </li>
+              <br />
+
+              
+      </div>
+    );
+  }
+}
+
+export default button;
